35e959882e840cd04589e8d6335656f26dfa6609
TGBenW <dukariga@gmail.com>
2023-03-18 00:36:02 +0200

    Simplify the boilerplate app

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+// module.exports = {
+//   esversion: 6,
+//   env: {
+//     browser: true,
+//     es2021: true,
+//   },
+//   extends: ['plugin:react/recommended', 'standard'],
+//   overrides: [],
+//   parserOptions: {
+//     ecmaVersion: 'latest',
+//     sourceType: 'module',
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//   },
+//   plugins: ['react'],
+//   rules: {},
+// };
+
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//   },
+//   extends: ['plugin:react/recommended', 'standard'],
+//   parserOptions: {
+//     ecmaVersion: 12, // Change 'latest' to the specific ECMAScript version, e.g., 12 for ES2021
+//     sourceType: 'module',
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//   },
+//   plugins: ['react'],
+//   rules: {},
+// };
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope': 'off', // Turn off the rule that requires React to be in scope
+    'react-hooks/rules-of-hooks': 'error', // Add the React Hooks rules
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the React version
+      runtime: 'automatic', // Use the new JSX runtime
+    },
+  },
+}
